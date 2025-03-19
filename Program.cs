0bd30800b403c63129a0b1ee3681fb3b53d04d6d
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 16:37:14 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!");
+﻿using System;
+
+namespace GenericMethod
+{
+    class SimpleDataBase<T>
+    {
+        public List<T> storedData;
+        public List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T data)
+        {
+            storedData.Add(data);
+            DateTime currentTime = DateTime.Now;
+            inputDates.Add(DateTime.Now);
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine("Data berisi: {0}, yang disimpan pada waktu UTC: {1}", storedData[i], inputDates[i]);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleDataBase<int> simple = new SimpleDataBase<int>();
+            simple.AddNewData(10);
+            simple.AddNewData(30);
+            simple.AddNewData(22);
+            simple.PrintAllData();
+        }
+    }
+}

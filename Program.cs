3cc29042580b6c49b23071767c8a6aa9a0a74358
RezaIndraMaulana <81007109+RezaIndraMaulana@users.noreply.github.com>
2025-03-19 16:46:48 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -29,6 +29,26 @@ namespace GenericMethod
         }
     }
 
+    public void DapatkanNilaiTerbesar<T>(T data1, T data2, T data3)
+{
+    dynamic d1 = data1;
+    dynamic d2 = data2;
+    dynamic d3 = data3;
+
+    if(d1 > d2 && d1 > d3)
+        {
+            Console.WriteLine("Data1: {0} adalah nilai terbesar", data1);
+        }
+        else if (d2 > d1 && d2 > d3)
+        {
+            Console.WriteLine("Data2: {0} adalah nilai terbesar", data2);
+        }
+        else
+        {
+            Console.WriteLine("Data3: {0} adalah nilai terbesar", data3);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -38,6 +58,9 @@ namespace GenericMethod
             simple.AddNewData(30);
             simple.AddNewData(22);
             simple.PrintAllData();
+
+            PemrosesData pemrosesData = new PemrosesData();
+            pemrosesData.DapatkanNilaiTerbesar<long>(10, 30, 22);
         }
     }
 }
